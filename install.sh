8b3dd18b84edc91d9d29a51c9b3ec6a2b9c38014
Pablo Caro <pablo@pcaro.es>
2013-06-29 00:26:11 +0200

    First remote commit.

--- install.sh
+++ install.sh
@@ -1,34 +1,119 @@
+#!/usr/bin/env bash
 
-	if [ -e ~/.vim ]
-		
-	fi
-	rm -rf ~/.vim ~/.vimrc
-	ln -s `pwd`/vim ~/.vim
-	ln -s ~/.vim/vimrc ~/.vimrc
+# ---------------------------------------------------
+# Copyright (c) 2013 Pablo Caro. All Rights Reserved.
+# Pablo Caro <pablo@pcaro.es> - http://pcaro.es/
+# install.sh
+# ---------------------------------------------------
 
-install-bash:
-	rm -f ~/.bashrc
-	ln -s `pwd`/bash/bashrc ~/.bashrc
+function backup {
+    echo -n "        "
+    mv -Tvf $HOME/.$1 $HOME/.$1.bak
+}
 
-install-git:
-	rm -f ~/.gitconfig
-	ln -s `pwd`/git/gitconfig ~/.gitconfig
+function link {
+    echo -n "        "
+    if [ $2 ] ; then
+        ln -vfs `pwd`/$2/$1 $HOME/.$1
+    else
+        ln -vfs `pwd`/$1 $HOME/.$1
+    fi
+}
 
-install-virtualenvwrapper:
-	mkdir -p ~/.virtualenvs
-	ln -s `pwd`/virtualenvwrapper/* ~/.virtualenvs
+function install_file {
+    echo "    - $1 -"
+    if [ -f $HOME/.$1 ] ; then
+        echo "        A .$1 file exists in your home folder."
+        echo "        Saving backup as .$1.bak"
+        backup $1
+        echo
+    fi
 
-install-python:
-	rm -f ~/.pythonstartup.py
-	ln -s `pwd`/python/pythonstartup.py ~/.pythonstartup.py
+    echo "        Linking $1"
+    link $1 $2
+    echo
+}
 
-dump-terminal-settings:
-	cp ~/Library/Preferences/com.apple.Terminal.plist terminal
-	plutil -convert xml1 terminal/com.apple.Terminal.plist
+function install_folder {
+    if [ -d $HOME/.$1 ] ; then
+        echo "        A .$1 file exists in your home folder."
+        echo "        Saving backup as .$1.bak"
+        backup $1
+        echo
+    fi
 
-install-terminal-settings:
-ifeq ($(shell uname),Darwin)
-	cp ~/Library/Preferences/com.apple.Terminal.plist terminal/old-settings.bak
-	cp terminal/com.apple.Terminal.plist ~/Library/Preferences
-	@echo "Old terminal settings were saved in terminal folder"
-endif
+    echo "        Linking $1"
+    link $1
+    echo
+}
+
+function install_bash {
+    echo "= Installing bash configuration files ="
+    echo "======================================="
+
+    folder="bash"
+
+    # Bash configuration
+    install_file "bashrc" $folder
+
+    # Bash alias definition
+    install_file "bash_alias" $folder
+
+}
+
+function install_git {
+    echo "= Installing git configuration files ="
+    echo "======================================"
+
+    folder="git"
+
+    # Git configuration
+    install_file "gitconfig" $folder
+
+    # Git global ignore list
+    install_file "gitignore_global" $folder
+
+}
+
+function install_vim {
+    echo "= Installing vim configuration files ="
+    echo "======================================"
+
+    folder="vim"
+
+    # Vim files folder
+    install_folder "vim"
+
+    # Vim config
+    install_file "vimrc" $folder
+
+    # Vim extra folders
+    mkdir vim/tmp
+    mkdir vim/bundle
+
+    # Install Vundle
+    git clone https://github.com/gmarik/vundle `pwd`/vim/bundle/vundle
+
+    # Check vim instalation
+    if [ `which vim` ] ; then
+        # Bundles instalation and activation
+        vim +BundleInstall +qall
+    fi
+
+}
+
+function install_others {
+    echo "= Installing other configuration files ="
+    echo "========================================"
+
+    folder="others"
+
+    # Ack configuration
+    install_file "ackrc" $folder
+
+}
+
+install_bash
+install_git
+install_vim
+install_others
