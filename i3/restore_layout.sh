f00039298fdbb0ab56b56583e489626c1a13d7a1
Pablo Caro <me@pcaro.es>
2014-06-16 22:32:58 +0200

    Using i3-save-tree

---
+++ i3/restore_layout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ----------------------------------------------------------------------
+# Copyright (c) 2014 Pablo Caro. All Rights Reserved.
+# Pablo Caro <me@pcaro.es> - https://pcaro.es/
+# restore_layout.sh
+# ----------------------------------------------------------------------
+
+i3-msg "workspace 3:IM; append_layout $HOME/.i3/workspace-3.json"
