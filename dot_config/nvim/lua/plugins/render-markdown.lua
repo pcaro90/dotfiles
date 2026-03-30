c9afa148803dd31e47bd0f43b7d6665997b1d968
Pablo Caro <me@pcaro.es>
2026-03-30 17:56:59 +0200

    fix(nvim): use lsp completions in render-markdown

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,8 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   event = {"BufReadPost", "BufNewFile"},
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
   opts = {
-    completions = { blink = { enabled = true } },
+    completions = { lsp = { enabled = true } },
   },
 }
